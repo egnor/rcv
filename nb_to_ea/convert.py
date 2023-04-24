1cae7a3cca87417b9432b7dc340937b897b13ffb
Dan Egnor <egnor@ofb.net>
2023-04-23 23:13:26 -0700

    lots of tweaks to conversion

--- nb_to_ea/convert.py
+++ nb_to_ea/convert.py
@@ -7,8 +7,7 @@ from pathlib import Path
 
 import fire  # type: ignore
 
-
-BASIC_MAP = dict(
+IDENTITY_MAP = dict(
     nationbuilder_id="Source ID",
     prefix="Prefix",
     first_name="First Name",
@@ -17,18 +16,11 @@ BASIC_MAP = dict(
     suffix="Suffix",
 )
 
-"""
 MISC_MAP = dict(
-    note=
-    employer=
-    occupation=
-    sex=
-    recruiter_name=
-    point_person_name=
-    is_volunteer=
-    availability=
+    note="Notes",
+    employer="Employer Name",
+    occupation="Occupation Name",
 )
-"""
 
 NB_ADDR_TYPES = ["primary", "address", "billing", "mailing", "user_submitted"]
 
@@ -55,13 +47,15 @@ NB_DO_NOT_CALL = "do_not_call"
 NB_DO_NOT_CONTACT = "do_not_contact"
 NB_EMAIL_BAD_SUFFIX = "_is_bad"
 NB_EMAIL_OPT_IN = "email_opt_in"
+NB_FACEBOOK_USERNAME = "facebook_username"
 NB_FEDERAL_DO_NOT_CALL = "federal_donotcall"
+NB_ID = "nationbuilder_id"
 NB_MOBILE_BAD = "is_mobile_bad"
+NB_MOBILE_OPT_IN = "mobile_opt_in"
 NB_PHONE_NUMBER_SUFFIX = "_number"
-NB_PHONE_OPT_IN_SUFFIX = "_opt_in"
 NB_PHONE_TYPE_MOBILE = "mobile"
-
-EA_ACTIVIST_CODE = "Activist Code"
+NB_TWITTER_LOGIN = "twitter_login"
+NB_WEBSITE = "website"
 
 EA_EMAIL_ADDRESS = "Email Address"
 EA_EMAIL_STATUS = "Email Subscription Status"
@@ -71,25 +65,32 @@ EA_EMAIL_STATUS_UNSUBSCRIBED = "Unsubscribed"
 EA_EMAIL_TYPE = "Email Type"
 EA_EMAIL_TYPE_OTHER = "Other"
 EA_EMAIL_TYPE_PERSONAL = "Personal"
-
+EA_EXT_FACEBOOK_URL = "Facebook URL"
+EA_EXT_NATIONBUILDER_ID = "NationBuilder ID"
+EA_EXT_OTHER = "Other Website"
+EA_EXT_TWITTER_HANDLE = "Twitter Handle"
 EA_PHONE_NUMBER = "Phone"
-EA_PHONE_OPT = "SMS Opt-In Status"
-EA_PHONE_OPT_IN = "Opt-In"
-EA_PHONE_OPT_OUT = "Opt-Out"
-EA_PHONE_OPT_UNKNOWN = "Unknown"
 EA_PHONE_TYPE = "Phone Type"
 EA_PHONE_TYPE_OTHER = "Other"
+EA_SMS_OPT = "SMS Opt-In Status"
+EA_SMS_OPT_IN = "Opt-In"
+EA_SMS_OPT_OUT = "Opt-Out"
+EA_SMS_OPT_UNKNOWN = "Unknown"
 
 ALL_EA_FIELDS = [
-    *BASIC_MAP.values(),
+    *IDENTITY_MAP.values(),
     *ADDR_MAP.values(),
-    EA_ACTIVIST_CODE,
+    *MISC_MAP.values(),
+    EA_EXT_FACEBOOK_URL,
+    EA_EXT_NATIONBUILDER_ID,
+    EA_EXT_OTHER,
+    EA_EXT_TWITTER_HANDLE,
     EA_EMAIL_ADDRESS,
     EA_EMAIL_STATUS,
     EA_EMAIL_TYPE,
     EA_PHONE_NUMBER,
     EA_PHONE_TYPE,
-    EA_PHONE_OPT,
+    EA_SMS_OPT,
 ]
 
 
@@ -100,11 +101,11 @@ def main():
     fire.Fire(fire_main)
 
 
-def fire_main(*nb_csvs, ea_csv=None, limit=10, activist_code="Test"):
+def fire_main(*nb_csvs, ea_csv=None, limit=10):
     """Converts NationBuilder CSV(s) to EveryAction CSV(s)
 
     :param nb_csvs: NationBuilder CSV(s) to convert (searches . by default)
-    :param ea_csv: EveryAction CSV to write (default based on nb_path)
+    :param ea_csv: EveryAction CSV to write (default based on nb_csvs)
     """
 
     if nb_csvs:
@@ -127,13 +128,12 @@ def fire_main(*nb_csvs, ea_csv=None, limit=10, activist_code="Test"):
             np = nb_path.stem.split("-")
             np = [p for p in np if p not in ("", "nationbuilder", "export")]
             np.extend([f"limit{limit}"] if limit else [])
-            np.extend([activist_code] if activist_code else [])
             ea_path = nb_path.with_name(f"everyaction-{'-'.join(np)}.txt")
 
-        convert(nb_path, ea_path, limit=limit, activist_code=activist_code)
+        convert(nb_path, ea_path, limit=limit)
 
 
-def convert(nb_path, ea_path, *, limit, activist_code):
+def convert(nb_path, ea_path, *, limit):
     """Converts NationBuilder CSV to EveryAction CSV
 
     :param nb_path: Path to NationBuilder CSV
@@ -157,11 +157,14 @@ def convert(nb_path, ea_path, *, limit, activist_code):
                 if limit and input_count >= limit:
                     print(f"🛑 Stopped at {limit} -> {output_count} rows")
                     break
-                for out_row in convert_row(nb_row, activist_code=activist_code):
-                    ea_writer.writerow({
-                        k: v.replace("\t", " ").replace("&", " ").strip()
-                        for k, v in out_row.items() if v
-                    })
+                for out_row in convert_row(nb_row):
+                    ea_writer.writerow(
+                        {
+                            k: v.replace("\t", " ").replace("&", "&").strip()
+                            for k, v in out_row.items()
+                            if v
+                        }
+                    )
                     output_count += 1
             else:
                 print(f"✅ {input_count} -> {output_count} rows")
@@ -169,21 +172,52 @@ def convert(nb_path, ea_path, *, limit, activist_code):
     print()
 
 
-def convert_row(nb_row, *, activist_code):
+def convert_row(nb_row):
     """Converts a NationBuilder row to EveryAction row
 
     :param nb_row: NationBuilder row as dict
     :return: EveryAction row(s) as sequence of dicts
     """
 
-    # QUESTIONS
-    # - How to tell organizations from individuals?
+    identity = {ek: nb_row.get(nk, "") for nk, ek in IDENTITY_MAP.items()}
+    identity[EA_EMAIL_TYPE] = EA_EMAIL_TYPE_OTHER
+    identity[EA_EMAIL_STATUS] = EA_EMAIL_STATUS_NOT_SUBSCRIBED
+    identity[EA_PHONE_TYPE] = EA_PHONE_TYPE_OTHER
+    identity[EA_SMS_OPT] = EA_SMS_OPT_UNKNOWN
 
-    basic = {ek: nb_row.get(nk, "") for nk, ek in BASIC_MAP.items()}
-    basic.update({EA_ACTIVIST_CODE: activist_code} if activist_code else {})
-    basic[EA_EMAIL_TYPE] = EA_EMAIL_TYPE_OTHER
-    basic[EA_PHONE_TYPE] = EA_PHONE_TYPE_OTHER
-    basic[EA_PHONE_OPT] = EA_PHONE_OPT_UNKNOWN
+    nb_no_call = convert_bool(nb_row.get(NB_DO_NOT_CALL))
+    nb_no_contact = convert_bool(nb_row.get(NB_DO_NOT_CONTACT))
+    nb_fed_no_call = convert_bool(nb_row.get(NB_FEDERAL_DO_NOT_CALL))
+    nb_mobile_bad = convert_bool(nb_row.get(NB_MOBILE_BAD))
+    nb_mobile_opt_in = convert_bool(nb_row.get(NB_MOBILE_OPT_IN))
+    if nb_no_contact or nb_no_call or nb_fed_no_call or nb_mobile_bad:
+        identity[EA_SMS_OPT] = EA_SMS_OPT_OUT
+    elif nb_mobile_opt_in is True:
+        identity[EA_SMS_OPT] = EA_SMS_OPT_IN
+    elif nb_mobile_opt_in is False:
+        identity[EA_SMS_OPT] = EA_SMS_OPT_OUT
+
+    misc = {ek: nb_row.get(nk, "") for nk, ek in MISC_MAP.items()}
+    misc[EA_EXT_NATIONBUILDER_ID] = nb_row.get(NB_ID)
+
+    website = nb_row.get(NB_WEBSITE)
+    if website:
+        twitter_match = re.match(r"^https?://twitter.com/(\w*)$", website)
+        if twitter_match:
+            misc[EA_EXT_TWITTER_HANDLE] = twitter_match.group(1)
+        elif re.match(r"^https?://www.facebook.com/", website):
+            misc[EA_EXT_FACEBOOK_URL] = website
+        else:
+            misc[EA_EXT_OTHER] = website
+
+    facebook_user = nb_row.get(NB_FACEBOOK_USERNAME)
+    if facebook_user:
+        facebook_user = facebook_user.strip("/").split("/")[-1]
+        misc[EA_EXT_FACEBOOK_URL] = f"https://www.facebook.com/{facebook_user}"
+
+    twitter_login = nb_row.get(NB_TWITTER_LOGIN)
+    if twitter_login:
+        misc[EA_EXT_TWITTER_HANDLE] = twitter_login.strip("@")
 
     addr_maps = []
     for nb_atype in NB_ADDR_TYPES:
@@ -191,60 +225,58 @@ def convert_row(nb_row, *, activist_code):
         if any(amap.values()) and amap not in addr_maps:
             addr_maps.append(amap)
 
-    nb_email_opt_in = (nb_row.get(NB_EMAIL_OPT_IN) == "true")
-    nb_no_contact = (nb_row.get(NB_DO_NOT_CONTACT) == "true")
+    nb_email_opt_in = convert_bool(nb_row.get(NB_EMAIL_OPT_IN))
     email_maps = []
     for nb_etype in NB_EMAIL_TYPES:
         email = nb_row.get(nb_etype)
-        nb_bad = (nb_row.get(f"{nb_etype}{NB_EMAIL_BAD_SUFFIX}") == "true")
+        nb_bad = convert_bool(nb_row.get(f"{nb_etype}{NB_EMAIL_BAD_SUFFIX}"))
         if email and all(email != m[EA_EMAIL_ADDRESS] for m in email_maps):
             ea_status = (
-                EA_EMAIL_STATUS_UNSUBSCRIBED if nb_bad or nb_no_contact else
-                EA_EMAIL_STATUS_SUBSCRIBED if nb_email_opt_in else
-                EA_EMAIL_STATUS_NOT_SUBSCRIBED
+                EA_EMAIL_STATUS_UNSUBSCRIBED
+                if nb_bad or nb_no_contact
+                else EA_EMAIL_STATUS_SUBSCRIBED
+                if nb_email_opt_in
+                else EA_EMAIL_STATUS_NOT_SUBSCRIBED
             )
-            email_maps.append({
-                EA_EMAIL_ADDRESS: email,
-                EA_EMAIL_TYPE: EA_EMAIL_TYPE_PERSONAL,
-                EA_EMAIL_STATUS: ea_status,
-            })
+            email_maps.append(
+                {
+                    EA_EMAIL_ADDRESS: email,
+                    EA_EMAIL_TYPE: EA_EMAIL_TYPE_PERSONAL,
+                    EA_EMAIL_STATUS: ea_status,
+                }
+            )
 
-    nb_no_call = (nb_row.get(NB_DO_NOT_CALL) == "true")
-    nb_fed_no_call = (nb_row.get(NB_FEDERAL_DO_NOT_CALL) == "true")
-    nb_mobile_bad = (nb_row.get(NB_MOBILE_BAD) == "true")
     phone_maps = []
     for nb_ptype, ea_ptype in PHONE_TYPE_MAP.items():
-        number = nb_row.get(f"{nb_ptype}{NB_PHONE_NUMBER_SUFFIX}")
-        opt_in = nb_row.get(f"{nb_ptype}{NB_PHONE_OPT_IN_SUFFIX}")
-        if number:
-            # Strip US country code and non-digits
-            number = re.sub(r"[^\d]", "", number)
-            number = re.sub(r"^1(\d{10})$", r"\1", number)
-            pmap = {EA_PHONE_NUMBER: number, EA_PHONE_TYPE: ea_ptype}
-            if nb_no_contact or nb_no_call or nb_fed_no_call:
-                pmap[EA_PHONE_OPT] = EA_PHONE_OPT_OUT
-            elif nb_mobile_bad and nb_ptype == NB_PHONE_TYPE_MOBILE:
-                pmap[EA_PHONE_OPT] = EA_PHONE_OPT_OUT
-            elif opt_in == "true":
-                pmap[EA_PHONE_OPT] = EA_PHONE_OPT_IN
-            elif opt_in == "false":
-                pmap[EA_PHONE_OPT] = EA_PHONE_OPT_OUT
+        # Strip US country code and non-digits
+        value = nb_row.get(f"{nb_ptype}{NB_PHONE_NUMBER_SUFFIX}", "")
+        digits = re.sub(r"[^\d]", "", value)
+        digits = re.sub(r"^1(\d{10})$", r"\1", digits)
+        if digits and "@" not in value:
+            pmap = {EA_PHONE_NUMBER: digits, EA_PHONE_TYPE: ea_ptype}
             if pmap not in phone_maps:
                 phone_maps.append(pmap)
 
-    """
-    yield {
-        **basic,
-        **(addr_maps.pop(0) if addr_maps else {}),
-        **(email_maps.pop(0) if email_maps else {}),
-        **(phone_maps.pop(0) if phone_maps else {}),
-    }
+    yield {**identity, **misc}
+    for addr_map in addr_maps:
+        yield {**identity, **addr_map}
+    for email_map in email_maps:
+        yield {**identity, **email_map}
+    for phone_map in phone_maps:
+        yield {**identity, **phone_map}
+
+
+def convert_bool(value):
+    """Converts text value from CSV to boolean
+
+    :param value: Value to convert
+    :return: Boolean value
     """
 
-    yield basic
-    for addr_map in addr_maps:
-        yield {**basic, **addr_map}
-    for email_map in email_maps:
-        yield {**basic, **email_map}
-    for phone_map in phone_maps:
-        yield {**basic, **phone_map}
+    value = value.lower() if isinstance(value, str) else value
+    return (
+        True if value in ("true", "yes", "y", "1", 1, True) else
+        False if value in ("false", "no", "n", "0", 0, False) else
+        None if value in ("", "na", "n/a", None) else
+        bool(value)
+    )
